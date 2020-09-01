d47d32a45366ccdf89bba0cd81f21f28a7360e39
Gabriel <gabriel.bertao@hotmail.com>
2020-09-01 09:07:53 -0300

    Card Contents, NavbarIcon

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+//express import
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 
+//express initialize
 const app = express()
 
+//ejs config
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname,'src','app','views'))
+
+app.use(express.static(path.join(__dirname,'src','public')))
+
+//route
 app.get("/", (req, res)=>{
-    res.send("ola mundo!")
+    res.render('homepage.ejs')
 })
 
 app.listen(3000)
